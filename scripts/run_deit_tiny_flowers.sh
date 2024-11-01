d7de91c2eea51464bf38310ab5674f2147a9932c
phemiloyeAI <oyerindefemiloye@gmail.com>
2024-11-01 17:00:27 +0100

    training scripts

--- scripts/run_deit_tiny_flowers.sh
+++ scripts/run_deit_tiny_flowers.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+
+load_cfg() {
+    cfg_file=$1
+    if [ -f "$cfg_file" ]; then
+        project_name=$(yq e '.model.project_name' "$cfg_file")
+        model_arch=$(yq e '.model.model_arch' "$cfg_file")
+        epochs=$(yq e '.model.epochs' "$cfg_file")
+        bs=$(yq e '.model.batch_size' "$cfg_file")
+        inp_s=$(yq e '.model.input_size' "$cfg_file")
+        lr=$(yq e '.model.learning_rate' "$cfg_file")
+        wd=$(yq e '.model.weight_decay' "$cfg_file")
+        alpha=$(yq e '.model.alpha' "$cfg_file")
+        embed_dim=$(yq e '.model.embed_dim' "$cfg_file")
+        pretrained=$(yq e '.ckpt_path' "$cfg_file")
+        
+        echo Loaded configurations
+    else
+        echo Configuration file does not exist
+        exit 1
+    fi 
+}
+load_cfg $1 
+
 python main_deit.py \
-    --gpus 8,9,10,11 \
-    -a deit_tiny_patch16_224 \
-    --dist-url 'tcp://localhost:10003' --dist-backend   'nccl' \
-    --multiprocessing-distributed  --world-size 1 --rank 0  \
-    --pretrained checkpoints/instance_discrimination/flowers2/deit_tiny_patch16_224_lr_0.0005_wd_0.001_bs_256_epochs_800_dim_192_path_flowers2cutmix_0.5crops_112_2_112_4/checkpoint_0799.pth.tar \
-    -j 16 --wd 1e-3 --lr 5e-4 \
-    --embed-dim 192 --num-classes 102 \
-    -b 256 --alpha 0.5 --epochs 200  /opt/caoyh/datasets/flowers2
+    -a $model_arch --project_name $project_name \
+    --pretrained $pretrained \
+    -j 12 --wd $wd --lr $lr --input-size $inp_s  \
+    --embed-dim $embed_dim --num-classes 102 --train_trainval \
+    -b $bs --alpha 0.5 --epochs $epochs  --data_cfg $1
